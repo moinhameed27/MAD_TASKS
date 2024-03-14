f0a95938807d4913d3ac28172859fd3c5f51e96b
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-03-15 00:28:53 +0500

    Splash + Login Page + Toasts
    Task Showing combined splash activity, login page and toasts on clicking the button.

---
+++ 05_SLT_Combined/app/src/main/java/com/example/loginpage/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.example.loginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SecondActivity.this, "Muhammad Moin U Din", Toast.LENGTH_SHORT).show();
+            }
+        });
+        button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SecondActivity.this, "Java Android Development", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SecondActivity.this, "University of the Punjab", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    }
+}
