1af3f81d7a2b4c73e0809b0121c10e64484d96d6
Moin Hameed <139669999+moinhameed27@users.noreply.github.com>
2024-04-19 21:06:15 +0500

    Splash Activity

--- 03_Splash/settings.gradle.kts
+++ 03_Splash/settings.gradle.kts
@@ -3,7 +3,7 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-    }
+    } 
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
